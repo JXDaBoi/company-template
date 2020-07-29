aa3bc0026474b5eff334d2ead6b1161b9f591fe5
JXDaBoi <jingxiangdaboi@gmail.com>
2020-07-29 21:36:25 +0800

    Major changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,20 +5,19 @@
 
       PARAMETERS
       :logoURL  (URL for Logo)
-     -->
+    -->
     <NaviWrapper
-      imgURL="/img/lead-white-logo.png"
-      ></NaviWrapper>
+      imgURL="https://64.media.tumblr.com/659df02b71ed0fca9725f28cc671356b/tumblr_plbt6hv1ij1wuv92g_540.png"
+    ></NaviWrapper>
 
-    <div class='flex-1'>
-      <div class='flex'>
-        <Nuxt class="w-full h-screen overflow-y-auto" />
+    <div class="flex-1">
+      <div class="flex">
+        <Nuxt class="w-full h-screen overflow-y-auto"/>
       </div>
     </div>
-    <FooterWrapper />
+    <FooterWrapper/>
   </div>
 </template>
 <script>
-export default {
-}
+export default {};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -11,24 +11,22 @@
           :second_header ( Header 2 Text)
           :descriptions ( Descriptions )
         -->
-        <HeaderContent 
-          imgURL="/img/slider1.jpeg"
-          header_txt="Company Template by LEAD"
-          second_header="Second Header"
-          descriptions="Header Descriptions"
+        <HeaderContent
+          imgURL="https://64.media.tumblr.com/659df02b71ed0fca9725f28cc671356b/tumblr_plbt6hv1ij1wuv92g_540.png"
+          header_txt="王阿姨团队"
+          second_header="想月入过万吗？"
+          descriptions="加入我们的团队就对了"
         ></HeaderContent>
         <!-- 
           About Content 
 
           PARAMETERS
           :imgURL ( URL for about us )
-         -->
+        -->
         <AboutContent
-          imgURL="/img/about.jpg"
+          imgURL="https://64.media.tumblr.com/659df02b71ed0fca9725f28cc671356b/tumblr_plbt6hv1ij1wuv92g_540.png"
         >
-          <template v-slot:about>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolorum rerum hic exercitationem odit, soluta eaque eos corporis quam alias ipsum repudiandae blanditiis tempore eligendi numquam temporibus! Molestias, quos beatae?
-          </template>
+          <template v-slot:about>加入我们的团队就可以少奋斗一世纪， 别等了 :)</template>
         </AboutContent>
 
         <ServiceContent>
@@ -40,48 +38,20 @@
               PARAMETERS
 
               :imgURL (image url)
-             -->
-            <Service
-              imgURL="/img/icon1.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            -->
+            <Service imgURL="/img/icon1.png" title="帮阿姨和叔叔换灯泡">
+              <template v-slot:content>就纯粹帮阿姨们和叔叔们换灯泡，以换得肉体和心灵上的回报</template>
             </Service>
 
-            <Service
-              imgURL="/img/icon2.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
-            </Service>
-
-            <Service
-              imgURL="/img/icon3.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
-            </Service>
-
-            <Service
-              imgURL="/img/icon4.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            <Service imgURL="/img/icon2.png" title="陪阿姨和叔叔聊天">
+              <template v-slot:content>就纯粹和阿姨们和叔叔们吹水，聊聊心事，顺便可以得到心灵上的辅导</template>
             </Service>
           </template>
         </ServiceContent>
 
         <!-- 
           Portfolio
-         -->
+        -->
         <PortfolioContent>
           <template v-slot:portfolio>
             <!-- 
@@ -92,14 +62,13 @@
               :name ( Name of the Project )
               :project ( Name of Project Type)
               :projectURL ( Link Address to project)
-             -->
+            -->
             <Project
-                imgURL='/img/about.jpg'
-                name='Project Name'
-                project='Project Type'
-                projectURL='https://google.com'
-              />
-
+              imgURL="https://64.media.tumblr.com/659df02b71ed0fca9725f28cc671356b/tumblr_plbt6hv1ij1wuv92g_540.png"
+              name="Project Name"
+              project="Project Type"
+              projectURL="https://google.com"
+            />
           </template>
         </PortfolioContent>
         <ContactContent
@@ -113,6 +82,5 @@
   </div>
 </template>
 <script>
-export default {
-}
+export default {};
 </script>
